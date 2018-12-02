42e18753ac4c18d7d8b2552cfc28eacebb9e9246
MoerkFang <Moerk@live.cn>
2018-12-02 20:51:06 +0800

    Change

--- openCV/main.cpp
+++ openCV/main.cpp
@@ -1,20 +1,4 @@
-<<<<<<< HEAD
-#include "SharpnessScore.h"
-int main() {
-	float a, b, c;
-	a=SharpnessScore("Lenna.png");
-	b=SharpnessScore("1.jpg");
-	c=SharpnessScore("2.jpg");
-	waitKey();
-=======
-<<<<<<< HEAD
-#include "Video.h"
-int main() {
-	Video();
-=======
 #include "HW2.h"
 int main() {
 	HW2();
->>>>>>> 5773dbc017f2f282116d91a0c56e5eafe0a63954
->>>>>>> 554ae61f4328fcc3ea126f553d014dba8743be9d
 }
